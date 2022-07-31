61da7ca25a6f2eeb3e226bc33feffb3e43413c0e
Luan <luan.nascimento@venhapranuvem.com>
2022-07-31 14:32:50 -0300

    Heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -30,7 +30,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   console.error(err);
   return response.status(500).json({
     status: 'error',
-    message: 'Internal Server Error"',
+    message: 'Internal Server Error!"',
   });
 });
 
