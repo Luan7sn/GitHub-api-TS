1f1a008a3a45dff1d03bb6f62136235878f721be
Luan <luan.nascimento@venhapranuvem.com>
2022-07-31 16:07:22 -0300

    ...

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -1,8 +1,16 @@
 const tsConfig = require('./tsconfig.json');
 const tsConfigPaths = require('tsconfig-paths');
 
-const baseUrl = './dist'; // Either absolute or relative path. If relative it's resolved to current working directory.
+const baseUrl = './dist';
 tsConfigPaths.register({
   baseUrl,
   paths: tsConfig.compilerOptions.paths,
 });
+/* let { baseUrl, paths } = tsConfig.compilerOptions;
+for (path in paths) {
+  paths[path][0] = paths[path][0]
+    .replace("src", "dist")
+    .replace(".ts", ".js");
+}
+
+tsConfigPaths.register({ baseUrl, paths }); */
