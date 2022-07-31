65d85d72ec611efd8f5dad132f33713752b67149
Luan <luan.nascimento@venhapranuvem.com>
2022-07-31 16:36:40 -0300

    Heroku

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import challengeRoutes from '@modules/challenge/infra/http/routes/challenge.routes';
-//import challengeRoutes from '../../../../modules/challenge/infra/http/routes/challenge.routes'
+//import challengeRoutes from '@modules/challenge/infra/http/routes/challenge.routes';
+import challengeRoutes from '../../../../modules/challenge/infra/http/routes/challenge.routes'
 
 const routes = Router();
 
